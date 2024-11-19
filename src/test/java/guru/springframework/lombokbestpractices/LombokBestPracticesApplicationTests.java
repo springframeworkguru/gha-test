425c49b4f3fa9d16d4ea73f6f54d963d73097af9
John Thompson <john@springframework.guru>
2024-11-19 13:50:02 -0500

    test pr

--- src/test/java/guru/springframework/lombokbestpractices/LombokBestPracticesApplicationTests.java
+++ src/test/java/guru/springframework/lombokbestpractices/LombokBestPracticesApplicationTests.java
@@ -3,6 +3,8 @@ package guru.springframework.lombokbestpractices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class LombokBestPracticesApplicationTests {
 
@@ -10,4 +12,9 @@ class LombokBestPracticesApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testFoo() {
+
+        assertEquals(1, 1);
+    }
 }
